81fef52b9530b9d9e44233ac0e467900e1f981c8
Nouwatin Jacob <jaysansa@gmail.com>
2018-03-27 16:17:50 +0100

    webpack and babel setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Index extends React.Component {
+  render() {    
+    return (
+      <div>
+       Hello React!
+        <h1>{1+1}</h1>    
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById("index"));
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
