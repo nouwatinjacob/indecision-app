0be0d5daa5a2076f90ffee3d2c33a1e6ee522d7b
Nouwatin Jacob <jaysansa@gmail.com>
2018-03-28 02:09:09 +0100

    splitting to component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,92 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class Index extends React.Component {
-  render() {    
-    return (
+class Indecision extends React.Component {
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hand of a computer';
+    const options = ['One', 'Two', 'Three'];
+    return(
       <div>
-       Hello React!
-        <h1>{1+1}</h1>    
+        <Header title={title} subtitle={subtitle}/>
+        <Action />
+        <Options options={options}/>
+        <AddOption />
+      </div>
+    ); 
+  }
+};
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
-}
+};
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  };
+  render() {
+    return (
+      <div>
+      <button onClick={this.handlePick}>What should i do?</button>
+      </div>
+    );
+  }
+};
 
+class Options extends React.Component {
+  handleRemoveAll() {
+    alert('Remove All');
+  };
+  render() {
+    return(
+      <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option} />)
+        }
+      </div>
+    );
+  }
+};
+
+class Option extends React.Component {
+  render() {
+    return(
+      <div>
+        {this.props.optionText}
+        <button>Remove</button>
+      </div>
+    );
+  }
+};
+
+class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    if(option){
+      alert(option);
+      e.target.elements.option.value = '';
+    }
+  };
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<Indecision />, document.getElementById("index"));
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,56 @@
- const addNum = (a,b) => a+b;
+import React from "react";
+import ReactDOM from "react-dom";
 
- export default addNum; 
+
+  const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hand of a computer',
+    options: []
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if(option) {
+      app.options.push(option);
+      e.target.elements.option.value = '';
+    }
+    render();
+  };
+  
+  const onRemoveAll = () => {
+    app.options = [];
+    render();
+  };
+
+  const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+  };
+  
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No option'}</p>      
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should i do?</button>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+ReactDOM.render(template, document.getElementById("index"));
+};
+
+render();
