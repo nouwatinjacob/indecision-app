70f245930b153b41c5d2add8503491fb7c039340
Nouwatin Jacob <jaysansa@gmail.com>
2018-04-09 12:36:21 +0100

    setting up webpack with scss

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [ 
+          'style-loader', 
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
